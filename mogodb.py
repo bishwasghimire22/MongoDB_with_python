d481bbc06d45371c0673912d158dadf66c80f626
Bishwas Ghimire <biswasghimire220@gmail.com>
2025-10-27 17:54:22 +0200

    Initial commit

---
+++ mogodb.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+# Change "library" to match the name of your database
+db = client["library"]
+# Change the collection name to match the name of your collection
+books = db.books.find()
+
+for book in books:
+    # Documents are generally turned into dictionaries in which there's a key for each field
+    # The following code prints the value of the "title" field
+    print(book["title"])
+    # Try printing all the fields of a document
