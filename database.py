c036f6f742aaa593d1a8ef04f0aa66b8146dab3a
Bishwas Ghimire <biswasghimire220@gmail.com>
2025-11-08 15:24:17 +0200

    Script to update database

--- database.py
+++ database.py
@@ -3,14 +3,8 @@ import os
 
 MONGO_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017/")
 
-client= MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI)
 # Change "library" to match the name of your database
 db = client["library"]
 # Change the collection name to match the name of your collection
 books = db.books.find()
-
-for book in books:
-    # Documents are generally turned into dictionaries in which there's a key for each field
-    # The following code prints the value of the "title" field
-    print(book["_id"], ":", book["title"])
-    # Try printing all the fields of a document
